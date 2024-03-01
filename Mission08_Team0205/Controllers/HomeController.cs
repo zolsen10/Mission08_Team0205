4bfbae832efa52ba3c2a8d01e10031742a2bb52b
zolsen10 <soccerzack10@yahoo.com>
2024-03-01 11:06:34 -0700

    merge

--- Mission08_Team0205/Controllers/HomeController.cs
+++ Mission08_Team0205/Controllers/HomeController.cs
@@ -1,16 +1,18 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Mission08_Team0205.Models;
-using System.Diagnostics;
 
 namespace Mission08_Team0205.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ITaskRepository _taskRepository;
+        private readonly ICategoryRepository _categoryRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ITaskRepository taskRepository, ICategoryRepository categoryRepository)
         {
-            _logger = logger;
+            _taskRepository = taskRepository;
+            _categoryRepository = categoryRepository;
         }
 
         public IActionResult Index()
@@ -18,15 +20,65 @@ namespace Mission08_Team0205.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public IActionResult Task()
         {
+            ViewBag.Categories = _categoryRepository.GetAllCategories();
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpPost]
+        public IActionResult Task(Task task)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (ModelState.IsValid)
+            {
+                _taskRepository.AddTask(task);
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Categories = _categoryRepository.GetAllCategories();
+            return View(task);
+        }
+
+        public IActionResult Quadrants()
+        {
+            var tasks = _taskRepository.GetIncompleteTasks();
+            return View(tasks);
+        }
+
+        [HttpPost]
+        public IActionResult MarkCompleted(int taskId)
+        {
+            _taskRepository.MarkTaskCompleted(taskId);
+            return RedirectToAction("Quadrants");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int taskId)
+        {
+            var task = _taskRepository.GetTaskById(taskId);
+            ViewBag.Categories = _categoryRepository.GetAllCategories();
+            return View(task);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                _taskRepository.UpdateTask(task);
+                return RedirectToAction("Quadrants");
+            }
+
+            ViewBag.Categories = _categoryRepository.GetAllCategories();
+            return View(task);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int taskId)
+        {
+            _taskRepository.DeleteTask(taskId);
+            return RedirectToAction("Quadrants");
         }
     }
 }
